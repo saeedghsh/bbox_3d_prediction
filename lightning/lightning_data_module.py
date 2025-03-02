e6207ed3d149115c3e254c62693dcd43a0b60df9
Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>
2025-03-02 23:42:28 +0100

    Simplify input args of MultimodalDataModule
    Signed-off-by: Saeed Gholami Shahbandi <saeed.gh.sh@gmail.com>

--- lightning/lightning_data_module.py
+++ lightning/lightning_data_module.py
@@ -1,11 +1,11 @@
 """LightningDataModule handling train/validation/test splits and data loading."""
 
+from pathlib import Path
 from typing import Dict, Optional
 
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, Subset, random_split
 
-from config.config_schema import DataConfig, TrainingConfig
 from dataset_handler.dataset_handler import DatasetHandler, build_transform
 
 
@@ -14,17 +14,25 @@ class MultimodalDataModule(pl.LightningDataModule):  # pylint: disable=too-many-
 
     def __init__(
         self,
-        data_config: DataConfig,
-        training_config: TrainingConfig,
+        data_dir: Path,
+        split_test: float,
+        split_val: float,
+        batch_size: int,
         data_channels_order: Dict[str, str],
         backbone_channels_order: Dict[str, str],
     ) -> None:
+        # pylint: disable=too-many-arguments, too-many-positional-arguments
         super().__init__()
-        self._data_config = data_config
-        self._training_config = training_config
-        self._batch_size = training_config.batch_size
-        self._data_channels_order = data_channels_order
-        self._backbone_channels_order = backbone_channels_order
+        self._data_dir = data_dir
+        self._split_test = split_test
+        self._split_val = split_val
+        self._batch_size = batch_size
+        self._frame_transform = build_transform(
+            rgb_source_order=data_channels_order["rgb"],
+            pc_source_order=data_channels_order["pc"],
+            rgb_target_order=backbone_channels_order["rgb"],
+            pc_target_order=backbone_channels_order["pc"],
+        )
         self._train_dataset: Subset
         self._val_dataset: Subset
         self._test_dataset: Subset
@@ -34,21 +42,11 @@ class MultimodalDataModule(pl.LightningDataModule):  # pylint: disable=too-many-
 
     def setup(self, stage: Optional[str] = None) -> None:  # pylint: disable=unused-argument
         """Set up datasets and define transforms based on model input requirements."""
-        full_dataset = DatasetHandler(
-            data_dir=self._data_config.dataset_dir,
-            transform=build_transform(
-                rgb_source_order=self._data_channels_order["rgb"],
-                pc_source_order=self._data_channels_order["pc"],
-                rgb_target_order=self._backbone_channels_order["rgb"],
-                pc_target_order=self._backbone_channels_order["pc"],
-            ),
-        )
-
+        full_dataset = DatasetHandler(data_dir=self._data_dir, transform=self._frame_transform)
         dataset_len = len(full_dataset)
-        test_size = int(dataset_len * self._data_config.split_test)
-        val_size = int(dataset_len * self._data_config.split_val)
+        test_size = int(dataset_len * self._split_test)
+        val_size = int(dataset_len * self._split_val)
         train_size = dataset_len - val_size - test_size
-
         self._train_dataset, self._val_dataset, self._test_dataset = random_split(
             full_dataset, [train_size, val_size, test_size]
         )
